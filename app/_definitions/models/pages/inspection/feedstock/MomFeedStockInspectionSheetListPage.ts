3abe8cf125786f9847fa2033c9371628348a6e45
Liuhaiwen <liuhaiwen@jimeng.io>
2024-11-01 14:38:50 +0800

    MOM-1791 【华特二期测试问题】进料检验审批中的单据禁止修改删除

--- app/_definitions/models/pages/inspection/feedstock/MomFeedStockInspectionSheetListPage.ts
+++ app/_definitions/models/pages/inspection/feedstock/MomFeedStockInspectionSheetListPage.ts
@@ -450,7 +450,7 @@ const page: RapidPage = {
           actionText: "修改",
           $permissionCheck: "tysInspectionFeedStock.manage",
           $exps: {
-            disabled: "$slot.record.approvalState !== 'approving' && $slot.record.approvalState !== 'uninitiated'",
+            disabled: "$slot.record.approvalState !== 'uninitiated'",
           },
         },
         {
@@ -462,7 +462,7 @@ const page: RapidPage = {
           entityCode: "MomInspectionSheet",
           $permissionCheck: "tysInspectionFeedStock.manage",
           $exps: {
-            disabled: "$slot.record.approvalState !== 'approving' && $slot.record.approvalState !== 'uninitiated'",
+            disabled: "$slot.record.approvalState !== 'uninitiated'",
           },
         },
         {
