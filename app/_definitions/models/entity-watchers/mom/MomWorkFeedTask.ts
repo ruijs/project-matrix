225e25d846b54e8c2acf36854699cd44c1750147
Tony.Shao <xiocode@gmail.com>
2024-11-15 10:43:54 +0800

    添加安全库存

--- app/_definitions/models/entity-watchers/mom/MomWorkFeedTask.ts
+++ app/_definitions/models/entity-watchers/mom/MomWorkFeedTask.ts
@@ -32,14 +32,24 @@ export default [
         throw new Error("工单任务正在执行中");
       }
 
+      if (before.hasOwnProperty('processes') && !before.hasOwnProperty('process')) {
+        if (before.processes.length > 0) {
+          before.process = before.processes[0];
+        }
+      }
+
+      if (before.hasOwnProperty('process') && !before.hasOwnProperty('processes')) {
+        before.processes = [before?.process?.id || before?.process || before.process_id];
+      }
+
       if (!before.hasOwnProperty("workOrder") && !before.hasOwnProperty("work_order_id")) {
         const workOrderManager = server.getEntityManager<MomWorkOrder>("mom_work_order");
         const workOrder = await workOrderManager.findEntity({
           filters: [
             {
-              operator: "eq",
-              field: "process_id",
-              value: before.processes
+              operator: "exists",
+              field: "processes",
+              filters: [{ operator: "in", field: "id", value: before.processes }]
             },
             { operator: "eq", field: "executionState", value: 'processing' },
           ],
@@ -49,6 +59,9 @@ export default [
         }
       }
 
+      if (before.hasOwnProperty("processes")) {
+        delete before.processes;
+      }
     }
   },
   {
