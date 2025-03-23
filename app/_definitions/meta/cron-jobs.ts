a5f5b0e42e6883fce251574193ec2afa9348a1ca
Fossil <imzshh@gmail.com>
2025-03-23 16:10:00 +0800

    fix: meta files

--- app/_definitions/meta/cron-jobs.ts
+++ app/_definitions/meta/cron-jobs.ts
@@ -1,18 +1,20 @@
 import type { CronJobConfiguration as TCronJobConfiguration } from '@ruiapp/rapid-core';
 import detectOfflinePrinters from '../models/cron-jobs/detectOfflinePrinters';
+import kisRefreshAccessTokenJob from '../models/cron-jobs/kisRefreshAccessTokenJob';
+import kisRefreshAuthDataJob from '../models/cron-jobs/kisRefreshAuthDataJob';
 import kis_sync_audit_status from '../models/cron-jobs/kis_sync_audit_status';
 import kis_sync_base_data_job from '../models/cron-jobs/kis_sync_base_data_job';
 import kis_sync_inventory_data_job from '../models/cron-jobs/kis_sync_inventory_data_job';
 import kis_sync_inventory_notify_job from '../models/cron-jobs/kis_sync_inventory_notify_job';
-import kis_update_tokens_job from '../models/cron-jobs/kisRefreshAccessTokenJob';
 import update_inventory_balance from '../models/cron-jobs/update_inventory_balance';
 
 export default [
   detectOfflinePrinters,
+  kisRefreshAccessTokenJob,
+  kisRefreshAuthDataJob,
   kis_sync_audit_status,
   kis_sync_base_data_job,
   kis_sync_inventory_data_job,
   kis_sync_inventory_notify_job,
-  kis_update_tokens_job,
   update_inventory_balance,
 ] as TCronJobConfiguration[];
